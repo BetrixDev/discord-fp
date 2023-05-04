dd28eb97c2429b094c6f9e1e645c2acd82bac6cd
Ryan Bailey <rdbailey.dev@gmail.com>
2023-05-04 18:59:44 +0000

    Update 22 files

--- packages/discord-fp/dist/index.d.ts
+++ packages/discord-fp/dist/index.d.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, Client, ButtonInteraction, AnySelectMenuInteraction, StringSelectMenuInteraction, ChannelSelectMenuInteraction, MentionableSelectMenuInteraction, RoleSelectMenuInteraction, UserSelectMenuInteraction, ModalSubmitInteraction, Interaction, CommandInteractionOption, LocalizationMap, ChannelType, AutocompleteInteraction } from 'discord.js';
+import { CommandInteraction, Client, ClientEvents, ButtonInteraction, AnySelectMenuInteraction, StringSelectMenuInteraction, ChannelSelectMenuInteraction, MentionableSelectMenuInteraction, RoleSelectMenuInteraction, UserSelectMenuInteraction, ModalSubmitInteraction, Interaction, CommandInteractionOption, LocalizationMap, ChannelType, AutocompleteInteraction } from 'discord.js';
 import { Promisable } from 'type-fest';
 
 type ApplicationCommandOptionTypeString = "attachment" | "boolean" | "channel" | "integer" | "mentionable" | "number" | "user" | "string" | "subcommand" | "subcommandGroup";
@@ -42,6 +42,8 @@ interface DiscordFPHandlerOptions {
 declare function command<T extends CommandArguments>(options: CommandOptions<T>, run: (interaction: CommandInteraction, args: {
     [K in keyof T]: T[K]["required"] extends true ? ReturnTypeOfType<T[K]["type"]> : ReturnTypeOfType<T[K]["type"]> | undefined;
 }, client: Client) => Promisable<void>): void;
+declare function on<T extends keyof ClientEvents>(eventName: T, run: (...args: ClientEvents[T]) => Promisable<void>): void;
+declare function once<T extends keyof ClientEvents>(eventName: T, run: (...args: ClientEvents[T]) => Promisable<void>): void;
 declare function button(options: {
     id: RegExp | string;
 }, run: ComponentRunFunction<ButtonInteraction>): void;
@@ -52,7 +54,8 @@ declare function selectMenu<T extends SelectMenuTypeString>(options: {
 declare function modal(options: {
     id: RegExp | string;
 }, run: (interaction: ModalSubmitInteraction, client: Client) => void | Promise<void>): void;
+declare function middleware(run: (next: () => void, interaction: Interaction, client: Client) => void): (next: () => void, interaction: Interaction, client: Client) => void;
 declare function handleInteraction(interaction: Interaction): Promise<void>;
 declare function initInteractionHandler(discordClient: Client, options?: DiscordFPHandlerOptions): Promise<void>;
 
-export { DiscordFPHandlerOptions, button, command, handleInteraction, initInteractionHandler, modal, selectMenu };
+export { DiscordFPHandlerOptions, button, command, handleInteraction, initInteractionHandler, middleware, modal, on, once, selectMenu };
